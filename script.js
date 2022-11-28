6f5324698c59102413c7f6f53edc6c3363650f5c
Ronnie James James <119317722+ronniejamestorres@users.noreply.github.com>
2022-11-28 13:27:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+window.onload = () => {
+  const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+  for (let i = 0; i < tab_switchers.length; i++) {
+    const tab_switcher = tab_switchers[i];
+    const page_id = tab_switcher.dataset.tab;
+
+    tab_switcher.addEventListener('click', () => {
+      document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+      tab_switcher.parentNode.classList.add('is-active');
+
+      SwitchPage(page_id);
+
+    });
+  }
+}
+
+function SwitchPage(page_id) {
+  console.log(page_id);
+  const current_page = document.querySelector('.pages .page.is-active');
+  current_page.classList.remove('is-active');
+
+  const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+  next_page.classList.add('is-active');
+
+}
+
+//début animation
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+//fin animation
